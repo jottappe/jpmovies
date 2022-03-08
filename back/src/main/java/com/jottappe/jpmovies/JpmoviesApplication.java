22461886dfa70acd796c07efa37a9f0ecb0c4b8c
unknown <candido.jp@hotmail.com>
2022-03-08 20:04:18 -0300

    Project created.

---
+++ back/src/main/java/com/jottappe/jpmovies/JpmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.jottappe.jpmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpmoviesApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/jottappe/jpmovies/JpmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jottappe.jpmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
